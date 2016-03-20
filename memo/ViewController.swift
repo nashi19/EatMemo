34ede8f3230854c064f01e315e7c13314bcf077e
nashi19 <tnsc.tomoyuki@gmail.com>
2016-03-20 11:11:20 +0900

    Initial Commit

---
+++ memo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  memo
+//
+//  Created by takanashi tomoyuki on 2016/03/20.
+//  Copyright © 2016年 takanashi tomoyuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
