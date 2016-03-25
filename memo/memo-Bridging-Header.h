0a50bc79bcd5580509ca9a14b5f5d0fc285202e5
nashi19 <tnsc.tomoyuki@gmail.com>
2016-03-25 23:01:00 +0900

    Cocoa pod導入

---
+++ memo/memo-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  memo-Bridging-Header.h
+//  memo
+//
+//  Created by takanashi tomoyuki on 2016/03/25.
+//  Copyright © 2016年 takanashi tomoyuki. All rights reserved.
+//
+
+#ifndef memo_Bridging_Header_h
+#define memo_Bridging_Header_h
+
+#import <NCMB/NCMB.h>
+
+
+#endif /* memo_Bridging_Header_h */
